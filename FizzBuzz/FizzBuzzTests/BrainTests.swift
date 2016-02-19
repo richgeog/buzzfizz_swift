2fdbde50a29b8b3167ecf991868b62f7913095d7
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-19 23:56:23 +0000

    written test for numbers divisible by 3

--- FizzBuzz/FizzBuzzTests/BrainTests.swift
+++ FizzBuzz/FizzBuzzTests/BrainTests.swift
@@ -19,17 +19,10 @@ class BrainTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(3)
+        XCTAssertEqual(result, true)
     }
     
 }
