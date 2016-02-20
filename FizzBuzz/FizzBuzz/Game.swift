97e5f4b80546ab62e5ce0a9628d429431580f921
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-20 21:28:40 +0000

    added game test file and game class file

---
+++ FizzBuzz/FizzBuzz/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Richard Geoghegan on 20/02/2016.
+//  Copyright © 2016 Richard Geoghegan. All rights reserved.
+//
+
+import Foundation
+
+class Game: NSObject {
+    
+}
---
+++ FizzBuzz/FizzBuzzTests/GameTests.swift
@@ -0,0 +1,25 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Richard Geoghegan on 20/02/2016.
+//  Copyright © 2016 Richard Geoghegan. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+
+    override func setUp() {
+        super.setUp()
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
