2adc3e0686c20cd9d431b057e9ee4fa004dc5b4a
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-20 00:09:15 +0000

    function for divisible by 3 added and test passing

---
+++ FizzBuzz/FizzBuzz/Brain.swift
@@ -0,0 +1,16 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Richard Geoghegan on 19/02/2016.
+//  Copyright © 2016 Richard Geoghegan. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        return true
+    }
+}
