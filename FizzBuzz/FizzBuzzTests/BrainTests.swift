180c4634d71f2136de6030ddf55cb1436ea74b51
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-20 15:09:10 +0000

    added test for buzz function

--- FizzBuzz/FizzBuzzTests/BrainTests.swift
+++ FizzBuzz/FizzBuzzTests/BrainTests.swift
@@ -58,4 +58,9 @@ class BrainTests: XCTestCase {
         XCTAssertEqual(result, "Fizz")
     }
     
+    func testSayBuzz() {
+        let result = brain.check(5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
 }
